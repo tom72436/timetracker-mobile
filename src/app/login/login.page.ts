66e510eba20a44c255ba5d97db8350ab236035c2
Tom <tomagain@mail.com>
2024-01-16 15:49:16 +0100

    Added login input check

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
 
-  constructor() { }
+  username!: string;
+  password!: string;
 
-  ngOnInit() {
+  constructor(private router: Router) { }
+
+  login() {
+    if(this.username && this.password) {
+      console.log(this.username);
+      this.router.navigate(['/select-construction-area']);
+    }
+    else {
+      alert("Please enter a username and password");
+    }
   }
-
 }
