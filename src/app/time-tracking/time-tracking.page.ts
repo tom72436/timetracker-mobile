f4d96eb402df657bde592b4a6b8e6269337f98bf
Tom <tomagain@mail.com>
2024-01-18 08:11:29 +0100

    Implemented timer

--- src/app/time-tracking/time-tracking.page.ts
+++ src/app/time-tracking/time-tracking.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
 
 @Component({
@@ -6,11 +6,53 @@ import { PhotoService } from '../services/photo.service';
   templateUrl: './time-tracking.page.html',
   styleUrls: ['./time-tracking.page.scss'],
 })
-export class TimeTrackingPage {
+export class TimeTrackingPage implements OnDestroy {
+  timer: any;
+  timeInSeconds: number = 0;
+  todayWorked: Array<string> = [];
 
   constructor(public photoService: PhotoService) { }
+
+  ngOnDestroy() {
+    this.stopTimer();
+  }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
+
+  // this method starts the timer and increases it every second
+  startTimer() {
+    this.timer = setInterval(() => {
+      this.timeInSeconds++;
+    }, 1000);
+  }
+
+  // stops the tiimer and resets it
+  stopTimer() {
+    if (this.timeInSeconds !== 0) {
+      this.todayWorked.push(this.getTimeString());
+      clearInterval(this.timer);
+      this.resetTimer();
+    }
+  }
+
+  resetTimer() {
+    this.timeInSeconds = 0;
+  }
+
+  getTimeString() {
+    // calculate hours, minutes, and seconds
+    var hours = Math.floor(this.timeInSeconds / 3600);
+    var minutes = Math.floor((this.timeInSeconds % 3600) / 60);
+    var seconds = this.timeInSeconds % 60;
+
+    // format and return the time string
+    return `${this.formatTime(hours)}:${this.formatTime(minutes)}:${this.formatTime(seconds)}`;
+  }
+
+  private formatTime(time: number): string {
+    return time < 10 ? `0${time}` : `${time}`;
+  }
+
 }
